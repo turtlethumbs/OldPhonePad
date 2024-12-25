a5dba172b6f3516e79910d49edf7abacf6fccd26
turtlethumbs <alexander.r.shepherd@protonmail.com>
2024-12-26 00:22:56 +0700

    refactor(OldPhonePad): reorder functions in the order they are called

--- IronSoftware.OldPhonePad/LegagyReader.cs
+++ IronSoftware.OldPhonePad/LegagyReader.cs
@@ -44,6 +44,47 @@ namespace IronSoftware.OldPhonePad
             return transformedString;
         }
 
+        private static void ValidateInputIsNotEmpty(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentException("Input cannot be empty", nameof(input));
+        }
+
+        private static void ValidateInputHasLegalCharsOnly(string input)
+        {
+            foreach (char c in input)
+                if (!LEGAL_INPUT_CHARS.Contains(c))
+                    throw new ArgumentException("Input cannot be processed due to illegal char '" + c + "'", nameof(input));
+        }
+
+        private static void ValidateInputHasPoundSymbolAsFinalChar(string input)
+        {
+            char poundSymbol = '#';
+            int poundSymbolCount = input.Count(c => c == poundSymbol);
+            if (poundSymbolCount >= 2)
+                throw new ArgumentException("Input cannot be processed due to existence of multiple '" + poundSymbol + "' symbol chars", nameof(input));
+            if (poundSymbolCount == 0)
+                throw new ArgumentException("Input cannot be processed due to '" + poundSymbol + "' symbol char not found", nameof(input));
+            if (input.IndexOf(poundSymbol) != input.Length - 1)
+                throw new ArgumentException("Input cannot be processed due to '" + poundSymbol + "' symbol char not properly placed at end of input", nameof(input));
+        }
+
+        private static Dictionary<char, string> CreateKeypadMapping()
+        {
+            Dictionary<char, string> mapping = new Dictionary<char, string>();
+            mapping.Add('1', "&'(");
+            mapping.Add('2', "ABC");
+            mapping.Add('3', "DEF");
+            mapping.Add('4', "GHI");
+            mapping.Add('5', "JKL");
+            mapping.Add('6', "MNO");
+            mapping.Add('7', "PQRS");
+            mapping.Add('8', "TUV");
+            mapping.Add('9', "WXYZ");
+            mapping.Add('0', " ");
+            return mapping;
+        }
+
         private static string ReplaceParenthesisChar(string transformedString)
         {
             if (transformedString.Length <= 0) return transformedString;
@@ -63,52 +104,10 @@ namespace IronSoftware.OldPhonePad
                 {
                     transformedString += PARENTHESIS_R;
                     return transformedString;
-                }   
+                }
             }
             transformedString += PARENTHESIS_L;
             return transformedString;
-        }
-
-        private static Dictionary<char, string> CreateKeypadMapping()
-        {
-            Dictionary<char, string> mapping = new Dictionary<char, string>();
-            mapping.Add('1', "&'(");
-            mapping.Add('2', "ABC");
-            mapping.Add('3', "DEF");
-            mapping.Add('4', "GHI");
-            mapping.Add('5', "JKL");
-            mapping.Add('6', "MNO");
-            mapping.Add('7', "PQRS");
-            mapping.Add('8', "TUV");
-            mapping.Add('9', "WXYZ");
-            mapping.Add('0', " ");
-            return mapping;
-        }
-
-        private static void ValidateInputIsNotEmpty(string input)
-        {
-            if (string.IsNullOrEmpty(input))
-                throw new ArgumentException("Input cannot be empty", nameof(input));
-        }
-
-        private static void ValidateInputHasLegalCharsOnly(string input)
-        {
-            foreach (char c in input)
-                if (!LEGAL_INPUT_CHARS.Contains(c))
-                     throw new ArgumentException("Input cannot be processed due to illegal char '" + c + "'", nameof(input));
-
-        }
-
-        private static void ValidateInputHasPoundSymbolAsFinalChar(string input)
-        {
-            char poundSymbol = '#';
-            int poundSymbolCount = input.Count(c => c == poundSymbol);
-            if (poundSymbolCount >= 2)
-                throw new ArgumentException("Input cannot be processed due to existence of multiple '" + poundSymbol + "' symbol chars", nameof(input));
-            if (poundSymbolCount == 0)
-                throw new ArgumentException("Input cannot be processed due to '" + poundSymbol + "' symbol char not found", nameof(input));
-            if (input.IndexOf(poundSymbol) != input.Length - 1)
-                throw new ArgumentException("Input cannot be processed due to '" + poundSymbol + "' symbol char not properly placed at end of input", nameof(input));
         }
     }
 }
